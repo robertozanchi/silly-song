50587d6a33a343af45c421ce7640c7c21fa3b43e
robertozanchi <r.zanchi@gmail.com>
2015-12-01 22:49:59 +0100

    complete all compulsory tasks

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameField.delegate = self        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(sender: AnyObject) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(sender: AnyObject) {
+        if (nameField.text != nil) {
+            lyricsView.text = lyricsForName(bananaFanaTemplate, fullName: nameField.text!)
+        }
+        
+    }
 
 }
 
+func shortNameForName(name: String) -> String {
+    let lowercaseName = name.lowercaseString
+    let vowelSet = NSCharacterSet(charactersInString: "aeiou")
+    
+    if let firstVowelRange = lowercaseName.stringByFoldingWithOptions(.DiacriticInsensitiveSearch, locale: nil).rangeOfCharacterFromSet(vowelSet) {
+        return lowercaseName.substringFromIndex(firstVowelRange.startIndex)
+    }
+    
+    return lowercaseName
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joinWithSeparator("\n")
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    let shortName = shortNameForName(fullName)
+    
+    let lyrics = lyricsTemplate
+        .stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName)
+        .stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortName)
+    
+    return lyrics
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+}
